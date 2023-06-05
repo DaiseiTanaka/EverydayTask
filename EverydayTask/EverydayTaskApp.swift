a539dbfa3346914a083616af6526ab009d68c7f6
Daisei Tanaka <d.tnk.4004@gmail.com>
2023-06-05 13:41:49 +0900

    Initial Commit

---
+++ EverydayTask/EverydayTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverydayTaskApp.swift
+//  EverydayTask
+//
+//  Created by 田中大誓 on 2023/06/05.
+//
+
+import SwiftUI
+
+@main
+struct EverydayTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
